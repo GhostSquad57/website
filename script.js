2a0b29d0ff680679b91f1d04f81d0f8d48234634
Keith Henderson Jr <keithhendersonjr@gmail.com>
2018-04-08 18:18:56 -0400

    -Modified the site to be more mobile friendly
    -Removed server images, as they were big resource hogs
    
    -Added a JS script for handling the bar menu for mobile users

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleServers() {
+    var x = document.getElementById("servers");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
